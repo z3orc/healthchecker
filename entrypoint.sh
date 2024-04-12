11066581f39b3e4f71116483d97a92f9e2add4f8
z3orc <z3orc@users.noreply.github.com>
2024-04-12 23:51:48 +0200

    fix: wrong command format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 while :
 do
-    echo "curl -X POST ${HEALTHCHECK_URL}"
+    curl -s -X POST ${HEALTHCHECK_URL}
 
     if [ $? != 0 ] 
     then
         exit 1
     fi
 
-    sleep "${DELAY}"
+    sleep ${DELAY}
 done
