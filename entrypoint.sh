2675ecbe72e4277c408d534b36fff36685163e51
z3orc <z3orc@users.noreply.github.com>
2024-04-13 01:53:47 +0200

    feat: date and time for request

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 while :
 do
+    now=$(date +'%Y-%m-%dT%H:%M:%S%z')
     curl -s -X POST ${HEALTHCHECK_URL}
 
     if [ $? != 0 ] 
     then
+        echo "${date} Error while sending request"
         exit 1
     fi
+    echo "${date} Sending all-good signal to Healthcheck
 
     sleep ${DELAY}
 done
