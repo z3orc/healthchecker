fd29aaa63a99e135ce210995f8ec4bd8f5117c7d
z3orc <z3orc@users.noreply.github.com>
2024-04-13 02:36:17 +0200

    refactor: remove exit when curl fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ do
 
     if [ $? != 0 ] 
     then
-        echo "${now} Error while sending request"
-        exit 1
+        echo "${now} Error while sending request" 2>&1
+        # exit 1
     fi
-    echo "${now} Sending all-good signal to Healthcheck"
+    echo "${now} Sending all-good signal to Healthcheck" 2>&1
 
     sleep ${DELAY}
 done
